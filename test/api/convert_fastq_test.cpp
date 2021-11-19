95d1a2708c2bec0ebca03d9b01bc15b7d570c362
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2021-11-19 12:55:34 +0100

    [MISC] Update seqan3 submodule to 3.1.0 (#55)
    * [MISC] Update seqan3 submodule to 3.1.0
    
    * [INFRA] update doxygen version from 1.9.1 → 1.9.2
    
    * [INFRA] remove obsolete options from doxygen

--- test/api/convert_fastq_test.cpp
+++ test/api/convert_fastq_test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <seqan3/alphabet/detail/debug_stream_alphabet.hpp>
 #include <seqan3/alphabet/nucleotide/dna5.hpp>
-#include <seqan3/core/detail/debug_stream_alphabet.hpp>
 #include <seqan3/io/sequence_file/input.hpp>
 #include <seqan3/test/expect_range_eq.hpp>
 
