a4ca058a8a16052380b91e1fea0d7db9e824b78f
Enrico Seiler <enrico.seiler@hotmail.de>
2024-11-18 16:36:17 +0100

    [INFRA] Update CI

--- cmake/package-lock.cmake
+++ cmake/package-lock.cmake
@@ -6,10 +6,10 @@
 # This file should be committed to version control
 
 # hibf
-set (HIBF_VERSION 894f663c7b77f28cc2b1b8f4252e51c052c49bb2)
+set (HIBF_VERSION 1ce6960ce2400b53f3e64d2173b33d881d3358e0)
 CPMDeclarePackage (hibf
                    NAME hibf
-                   GIT_TAG ${HIBF_VERSION}
+                   GIT_TAG ${HIBF_VERSION} # main
                    GITHUB_REPOSITORY seqan/hibf
                    SYSTEM TRUE
                    EXCLUDE_FROM_ALL TRUE
@@ -17,10 +17,10 @@ CPMDeclarePackage (hibf
 )
 
 # sharg
-set (SHARG_VERSION 01f848eeb6dbac3d50a718a8ee26bcf695edf5a8)
+set (SHARG_VERSION c4367d1049322826e60c674b6bf24d3d0a8da999)
 CPMDeclarePackage (sharg
                    NAME sharg
-                   GIT_TAG ${SHARG_VERSION}
+                   GIT_TAG ${SHARG_VERSION} # main
                    GITHUB_REPOSITORY seqan/sharg-parser
                    SYSTEM TRUE
                    EXCLUDE_FROM_ALL TRUE
@@ -28,10 +28,10 @@ CPMDeclarePackage (sharg
 )
 
 # seqan3
-set (SEQAN3_VERSION fee3fb8e5976b455e848dd68c57f64fd3f67c3c2)
+set (SEQAN3_VERSION a79244729de1fb45b030cb7a4d8078f47f81d5f1)
 CPMDeclarePackage (seqan3
                    NAME seqan3
-                   GIT_TAG ${SEQAN3_VERSION}
+                   GIT_TAG ${SEQAN3_VERSION} # main
                    GITHUB_REPOSITORY seqan/seqan3
                    SYSTEM TRUE
                    EXCLUDE_FROM_ALL TRUE
@@ -39,10 +39,10 @@ CPMDeclarePackage (seqan3
 )
 
 # fmindex
-set (FMINDEX_VERSION 822d4d1aa6fa725f7d34e1ea819e9c23a2b6ebc2)
+set (FMINDEX_VERSION cd34f30d57926b5f4d9d10f54b1d2a8036fc59f7)
 CPMDeclarePackage (fmindex
                    NAME fmindex
-                   GIT_TAG ${FMINDEX_VERSION}
+                   GIT_TAG ${FMINDEX_VERSION} # main
                    GITHUB_REPOSITORY SGSSGene/fmindex-collection
                    SYSTEM TRUE
                    EXCLUDE_FROM_ALL TRUE
@@ -50,21 +50,22 @@ CPMDeclarePackage (fmindex
 )
 
 # googletest
-set (GOOGLETEST_VERSION 1.14.0)
+set (GOOGLETEST_VERSION 1.15.2)
 CPMDeclarePackage (googletest
                    NAME GTest
                    VERSION ${GOOGLETEST_VERSION}
                    GITHUB_REPOSITORY google/googletest
                    SYSTEM TRUE
                    EXCLUDE_FROM_ALL TRUE
-                   OPTIONS "BUILD_GMOCK OFF" "INSTALL_GTEST OFF" "CMAKE_MESSAGE_LOG_LEVEL WARNING"
+                   OPTIONS "BUILD_GMOCK OFF" "INSTALL_GTEST OFF" "CMAKE_CXX_STANDARD 20"
+                           "CMAKE_MESSAGE_LOG_LEVEL WARNING"
 )
 
 # use_ccache
 set (USE_CCACHE_VERSION d2a54ef555b6fc2d496a4c9506dbeb7cf899ce37)
 CPMDeclarePackage (use_ccache
                    NAME use_ccache
-                   GIT_TAG ${USE_CCACHE_VERSION}
+                   GIT_TAG ${USE_CCACHE_VERSION} # main
                    GITHUB_REPOSITORY seqan/cmake-scripts
                    SOURCE_SUBDIR ccache
                    SYSTEM TRUE
